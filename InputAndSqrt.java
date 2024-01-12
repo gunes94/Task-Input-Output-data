25b26c913ca1fc8ae4a48fb63356b55834a3bfd6
Gyunesh Mehmed <30699247+gunes94@users.noreply.github.com>
2024-01-12 17:25:35 +0200

    Update InputAndSqrt.java

--- InputAndSqrt.java
+++ InputAndSqrt.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 //Напишете програма, която чете 1 число от конзолата,
 //изчислява му корен квадратен и отпечатва резултата на екрана с точност до 5-тия знак след запетаята.
 
-public class Exercise_01_InputAndSqrt {
+public class InputAndSqrt {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
