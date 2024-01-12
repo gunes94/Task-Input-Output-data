7c15f73a652a00ccb8b2c70fc802089a411e1655
Gyunesh Mehmed <30699247+gunes94@users.noreply.github.com>
2024-01-12 17:25:14 +0200

    Update OddOrEven.java

--- OddOrEven.java
+++ OddOrEven.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 //Ако числото е четно, трябва да се изпише “even” (без кавичките),
 //а ако е нечетно - трябва да се изпише “odd”.
 
-public class Exercise_02_OddOrEven {
+public class OddOrEven {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
